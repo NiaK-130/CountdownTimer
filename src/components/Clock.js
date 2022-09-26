3f855790878ccc9463a9c4c9e4ee63f84390a459
Nia K <nsjkappen@gmail.com>
2022-09-26 07:47:08 -0700

    first commit

---
+++ src/components/Clock.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+import React from 'react'
+
+const Clock = ({timerDays, timerHours, timerMinutes, timerSeconds}) => {
+  return <Fragment>
+      
+
+
+  </Fragment>
+}
+
+export default Clock
